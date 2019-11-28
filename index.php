20ad3462bcb054a944e642db49e9452531ed2c83
Lukz <54214156+lucazfreitaz@users.noreply.github.com>
2019-11-28 17:07:01 -0300

    Primeiro pagina PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Titulo da página</title>
+    </head>
+    <body>
+        <h1>Página padrão PHP</h1>
+
+        <?php 
+        echo  "<p> Minha primeira conta</>" ;
+        echo '<p>';
+        echo 20;
+        echo '</p>';
+
+        ?>
+
+
+    </body>
+</html>
